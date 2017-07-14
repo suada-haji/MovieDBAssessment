f4a3e6e79784d2b02a8cde2abb1acc35c573c748
Suada Haji <suada.haji@andela.com>
2017-07-14 16:56:50 +0300

    Add poster path url

--- app/src/main/java/com/example/suadahaji/themovieapplication/util/Constants.java
+++ app/src/main/java/com/example/suadahaji/themovieapplication/util/Constants.java
@@ -10,4 +10,5 @@ public class Constants {
     public static final String API_KEY = BuildConfig.TMDB_KEY;
     public static final String BASE_URL = "https://api.themoviedb.org/3/";
     public static final String GET_POPULAR_MOVIES = "discover/movie?language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&api_key=" +  API_KEY;
+    public static final String POSTER_PATH = "https://image.tmdb.org/t/p/w500/";
 }
