13dd34943433f079e7653174f47cd043cbf878dc
Suada Haji <suada.haji@andela.com>
2017-07-14 18:55:16 +0300

    Add videos and reviews get requests

--- app/src/main/java/com/example/suadahaji/themovieapplication/api/ApiManager.java
+++ app/src/main/java/com/example/suadahaji/themovieapplication/api/ApiManager.java
@@ -1,10 +1,13 @@
 package com.example.suadahaji.themovieapplication.api;
 
 import com.example.suadahaji.themovieapplication.models.MovieResponse;
+import com.example.suadahaji.themovieapplication.models.ReviewResponse;
+import com.example.suadahaji.themovieapplication.models.VideoResponse;
 import com.example.suadahaji.themovieapplication.util.Constants;
 
 import io.reactivex.Observable;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 /**
  * Created by suadahaji
@@ -13,4 +16,11 @@ import retrofit2.http.GET;
 public interface ApiManager {
     @GET(Constants.GET_POPULAR_MOVIES)
     Observable<MovieResponse> getPopularMovies();
+
+    @GET(Constants.GET_REVIEWS)
+    Observable<ReviewResponse> getReviews(@Path("id") int id);
+
+    @GET(Constants.GET_VIDEOS)
+    Observable<VideoResponse> getVideos(@Path("id") int id);
+
 }
