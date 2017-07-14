7ff6aa219d3e4f0fb4b1849b77d2030a849df380
Suada Haji <suada.haji@andela.com>
2017-07-14 18:59:48 +0300

    Add videos and reviews constants

--- app/src/main/java/com/example/suadahaji/themovieapplication/util/Constants.java
+++ app/src/main/java/com/example/suadahaji/themovieapplication/util/Constants.java
@@ -11,4 +11,9 @@ public class Constants {
     public static final String BASE_URL = "http://api.themoviedb.org/3/";
     public static final String GET_POPULAR_MOVIES = "discover/movie?language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&api_key=" +  API_KEY;
     public static final String POSTER_PATH = "http://image.tmdb.org/t/p/w500";
+    public static final String GET_VIDEOS = "http://api.themoviedb.org/3/movie/{id}/videos?api_key=" + API_KEY;
+    public static final String GET_REVIEWS = "http://api.themoviedb.org/3/movie/{id}/reviews?api_key=" + API_KEY;
+    public static final String YOUTUBE_VIDEO_URL = "http://www.youtube.com/watch?v=";
+    public static final String YOUTUBE_THUMBNAIL_URL = "http://img.youtube.com/vi/";
+    public static final String MOVIE = "movie";
 }
